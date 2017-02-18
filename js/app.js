4b776783421d0aae6d229f606b0f7d1b02c76331
Wendy Liu <wendy_jy_liu@hotmail.com>
2017-02-18 02:06:48 -0800

    Migrated data from portfolio

---
+++ js/app.js
@@ -0,0 +1,15 @@
+﻿$('a[href*="#"]:not([href="#"])').click(function () {
+    // can use ' or " to wrap
+    // a[href*="#"] gets all anchors <a> that contains # in href
+    // but :not([href="#"]) exludes anchors with href exactly equal to #
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1300);
+            return false;
+        }
+    }
+});
